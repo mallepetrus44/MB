c21c1be73c35b25e8222ac0ea4bbe2d35b986c70
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-02-10 10:53:08 +0100

    register

---
+++ MB.Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MB.API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MB.Server.Areas.Identity.IdentityHostingStartup))]
+namespace MB.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
